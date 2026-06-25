9ec967690fe628c6c301f7190aa59bf157eee585
om-dhawale <219327955+om-dhawale@users.noreply.github.com>
2026-06-26 01:28:41 +0530

    fix dotenv override

--- db/redis.js
+++ db/redis.js
@@ -1,6 +1,3 @@
-console.log('ENV CHECK:', process.env.REDIS_PUBLIC_URL, process.env.JWT_SECRET);
 import Redis from 'ioredis';
-import dotenv from 'dotenv';
-dotenv.config({ override: false });
-const redis = new Redis(process.env.REDIS_PUBLIC_URL);
+const redis = new Redis(process.env.REDIS_PUBLIC_URL || 'redis://localhost:6379');
 export default redis;
